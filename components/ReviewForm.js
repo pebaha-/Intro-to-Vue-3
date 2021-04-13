9af4daa741141cd8559a6b31e9c4273abd7b82d3
Peter Bartram Hansen <pebaha@gmail.com>
2021-04-13 21:32:43 +0200

    Add ReviewForm.js

---
+++ components/ReviewForm.js
@@ -0,0 +1,32 @@
+app.component('review-form', {
+    props: {
+    },
+    template:
+    /*html*/
+    `<form class="review-form">
+        <h3>Leave a review</h3>
+        <label for="name">Name:</label>
+        <input id="name">
+
+        <label for="review">Review:</label>
+        <textarea id="review"></textarea>
+
+        <label for="rating">Rating:</label>
+        <select id="name">
+            <option>5</option>
+            <option>4</option>
+            <option>3</option>
+            <option>2</option>
+            <option>1</option>
+        </select>
+
+        <input class="button" type="submit" value="Submit">
+
+    </form>`,
+    data() {
+    },
+    methods: {
+    },
+    computed: {
+    }
+  })
