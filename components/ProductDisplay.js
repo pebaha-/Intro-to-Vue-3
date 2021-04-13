6db7d81664aeab69d195977e429912307c24945e
Peter Bartram Hansen <pebaha@gmail.com>
2021-04-13 20:38:02 +0200

    End of L9

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -17,9 +17,7 @@ app.component('product-display', {
                 <p v-if="inStock">In Stock</p>
                 <p v-else>Out of Stock</p>
                 <p>Shipping: {{ shipping }}</p>
-                <ul>
-                    <li v-for="detail in details">{{ detail }}</li>
-                </ul>
+                <product-details :details="details"></product-details>
                 <div
                     v-for="(variant, index) in variants" 
                     :key="variant.id"
@@ -35,8 +33,8 @@ app.component('product-display', {
         return {
             product: 'Socks',
             brand: 'Vue Mastery',
-            selectedVariant: 0,
             details: ['50% cotton', '30% wool', '20% polyester'],
+            selectedVariant: 0,
             variants: [
                 { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
                 { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
